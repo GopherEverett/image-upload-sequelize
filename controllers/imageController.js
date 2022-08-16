c2a66dbff576060baabc8b3085724d4fe75ff447
John Jacobs <john.a.jacobs1973@gmail.com>
2022-08-16 14:33:16 -0400

    working upload

---
+++ controllers/imageController.js
@@ -0,0 +1,51 @@
+const db = require('../models')
+const multer = require('multer')
+const path = require('path')
+const { Image } = require('../models')
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'Images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: '5000000' },
+    fileFilter: (req, file, cb) => {
+        const fileTypes = /jpeg|JPG|jpg|png|gif/
+        const mimeType = fileTypes.test(file.mimetype)
+        const extname = fileTypes.test(path.extname(file.originalname))
+
+        if (mimeType && extname) {
+            return cb(null, true)
+        }
+        cb('Give proper file format to upload')
+    }
+}).single('image')
+
+const addImage = async (req, res) => {
+    let info = {
+        image: req.file.path,
+        title: req.body.title
+    }
+    const image = await Image.create(info)
+    res.status(200).send(image)
+    console.log(image)
+}
+
+const getAllImages = async  (req, res) => {
+    const images = await Image.findAll()
+    res.status(200).send(images)
+}
+
+module.exports = {
+    storage,
+    upload,
+    addImage,
+    getAllImages
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+const app = express()
+const multer = require('multer')
+const path = require('path')
+const PORT = process.env.PORT || 3001
+const imageController = require('./controllers/imageController')
+
+
+
+
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(logger('dev'))
+
+
+
+app.get('/', imageController.getAllImages)
+app.post('/addImage', imageController.upload, imageController.addImage )
+
+app.use('/Images', express.static('./Images'))
+
+app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
